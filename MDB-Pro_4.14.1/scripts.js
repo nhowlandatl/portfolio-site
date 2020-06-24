431f2679797cdf511cef77da153c78dbbc78f684
Nick Howland <nhowland@outlook.com>
2020-06-23 20:38:22 -0400

    version 1 website

---
+++ MDB-Pro_4.14.1/scripts.js
@@ -0,0 +1,7 @@
+// all custom jQuery will go here
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      });
+  });
